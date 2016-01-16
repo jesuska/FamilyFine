233ca2253f2f0cdae976c6ff6971bc4565366714
jesuska <andras.szabo1982@gmail.com>
2016-01-16 23:04:15 +0100

    Add description column to Categories + create Test table with FK

--- SQLDatabase/dbo/Tables/Categories.sql
+++ SQLDatabase/dbo/Tables/Categories.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Categories] (
     [Id]   INT            IDENTITY (1, 1) NOT NULL,
     [Name] NVARCHAR (100) NOT NULL,
+	Description nvarchar(50) null,
     CONSTRAINT [PK_dbo.Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
---
+++ SQLDatabase/dbo/Tables/Test.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Test]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Description] VARCHAR(50) NOT NULL, 
+    [CategoryId] INT NOT NULL, 
+    CONSTRAINT [FK_Test_Categories] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id])
+)
